cb04c6114700359b6723a39c076e0b37d3d8e782
Shanice <shanicegriffin@pursuit.org>
2023-10-18 19:24:55 -0400

    changed some of the data in models

--- models/transactions.js
+++ models/transactions.js
@@ -16,14 +16,14 @@ module.exports = [
   {
     itemName: "Mom",
     date: "2/14/24",
-    amount: 150,
+    amount: -150,
     from: "Resturaunt",
     category: "Valentine's Day",
   },
   {
     itemName: "Coffee",
     date: "10/3/24",
-    amount: 2000,
+    amount: -10.75,
     from: "Starbucks",
     category: "Food",
   },
