1d767296dc8f848daa0e0037bca4c029edf7ccbf
Shanice <shanicegriffin@pursuit.org>
2023-10-22 20:48:28 -0400

    update server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app.js");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 8080 ;
 
 app.listen(PORT, () => {
     console.log(`%%%%%%%% We are live on port ${PORT} %%%%%%%%`)
